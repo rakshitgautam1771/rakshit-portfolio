c75f44389079963f5b30888865f1aca0ecdf3481
rakshitgautam1771 <rakshitgautam1771@gmail.com>
2026-07-30 13:22:31 +0530

    Redesign contact section

--- src/section/hero/hero.tsx
+++ src/section/hero/hero.tsx
@@ -2,28 +2,22 @@ import "./hero.css";
 
 import Button from "../../components/button/Button";
 import Container from "../../components/container/Container";
-
+import Resume from "../../assets/Rakshit Gautam Resume.pdf";
 import ProfileImage from "../../assets/profile.jpeg";
 
 const Hero = () => {
   return (
     <section className="hero">
-
       <Container>
-
         <div className="hero-wrapper">
-
           {/* Left Content */}
           <div className="hero-content">
-
             <div className="availability">
               <span className="availability-dot"></span>
               Available for Full-Time Opportunities
             </div>
 
-            <p className="hero-subtitle">
-              UI / UX DESIGNER
-            </p>
+            <p className="hero-subtitle">UI / UX DESIGNER</p>
 
             <h1 className="hero-title">
               Designing Digital
@@ -39,28 +33,28 @@ const Hero = () => {
             </p>
 
             <div className="hero-actions">
-              <Button><a href="#work">View Projects</a></Button>
-              <Button className="btn btn-secondary">Download Resume</Button>
+              <Button>
+                <a href="#work">View Projects</a>
+              </Button>
+             <a href={Resume} download="Rakshit_Gautam_Resume.pdf">
+  <Button className="btn btn-secondary">
+    Download Resume
+  </Button>
+</a>
             </div>
-
           </div>
 
           {/* Right Image */}
 
           <div className="hero-image-wrapper">
-
             <img
               src={ProfileImage}
               alt="Rakshit Gautam"
               className="hero-image"
             />
-
           </div>
-
         </div>
-
       </Container>
-
     </section>
   );
 };
